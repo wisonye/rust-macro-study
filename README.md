9c498ec9b7f0ddc7ea45ac756f70a366c3b8dfec
“Wison Ye” <wisonye@gmail.com>
2020-05-03 12:08:18 +1200

    [ master ] - My first macro to implement 'Login' trait works, test passed.

--- README.md
+++ README.md
@@ -13,3 +13,30 @@
     # Hot-load for `my-macro-consumer` package
     npm run start-consumer
     ```
+</br>
+<hr>
+
+## Add `cargo-expand` for doing a quick debug to our customized macro
+
+1. Install
+
+    ```bash
+    # Install `cargo-expand`
+    cargo install cargo-expand
+
+    # Install the nightly version, as `cargo-expand` needed.
+    rustup toolchain install nightly-x86_64-apple-darwin
+    ```
+
+2. How to run
+
+    ```bash
+    # `cargo expand` requires running against an actual package in this workspace
+    cd my-macro-lib/
+
+    # From now on, every time you want to check What content your marco expand (to real rust), just run:
+    cargo expand
+
+    # If you want to expand the test case inside your lib, then run :
+    cargo expand --lib --tests
+    ```
--- my-macro-lib/src/lib.rs
+++ my-macro-lib/src/lib.rs
@@ -1,7 +1,40 @@
+///
+pub trait Login {
+    fn login(&self, user_name: &str, password: &str) -> bool;
+}
+
+///
+macro_rules! impl_login {
+    ($target_struct: ty) => {{
+        impl Login for $target_struct {
+            fn login(&self, user_name: &str, password: &str) -> bool {
+                println!(
+                    "login method get called with user_name: '{}' and password: '{}'",
+                    user_name, password
+                );
+                true
+            }
+        }
+    }};
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn empty_login() {
+        struct MyTestStruct {}
+
+        // After calling the `impl_login!` macro, the `MyTestStruct` should
+        // be already implemented the `Login` trait which allows us to call
+        // `.login()` and return a `true`.
+        impl_login!(MyTestStruct);
+
+        let test_struct = MyTestStruct {};
+        let user_name = "wison";
+        let password = "my_password";
+        let login_result = test_struct.login(&user_name, &password);
+        assert_eq!(login_result, true);
     }
 }
